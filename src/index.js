9f600cc55595517d41665dc228972bdd2649074e
Tomasz Jakut <vepomoc@gmail.com>
2018-02-20 23:21:42 +0100

    feat(*): implement basic generation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-function doco() {
+import ESDoc from 'esdoc';
+import generateConfig from './generateConfig.js';
 
+function doco() {
+	return new Promise( ( resolve, reject ) => {
+		try {
+			const config = generateConfig();
+
+			ESDoc.generate( config );
+			resolve();
+		} catch ( error ) {
+			reject( error );
+		}
+	} );
 }
 
 export default doco;
